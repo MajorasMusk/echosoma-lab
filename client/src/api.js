1ed2004b57c3d3e6026e4c1f14269dd2c3282e37
MajorasMusk <zybez222@gmail.com>
2025-09-20 19:12:16 -0500

    update1

---
+++ client/src/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API = "http://localhost:8000"; // backend URL
+
+export async function runSigil(text) {
+  const res = await axios.post(`${API}/sigil?text=${text}`);
+  return res.data;
+}
+
+export async function getArchive() {
+  const res = await axios.get(`${API}/archive`);
+  return res.data;
+}
